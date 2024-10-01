00f7f3cf8884579defdf6dd4912cfa5991ab49c7
0109-11 <0109-11@softsol.local>
2024-10-01 15:44:08 +0300

    test

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok(new { status = "healthy" });
+        }
+    }
+}
